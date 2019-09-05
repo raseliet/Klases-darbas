c8b0454495d35687b202d418b3aab5ebc9256959
raseliet <54890928+raseliet@users.noreply.github.com>
2019-09-05 10:29:42 +0300

    Uzduotis - teksto pakeitimas i didziasias raides.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$text = 'Convert all characters to uppercase.The strtoupper() function converts a string to uppercase.
+
+Note: This function is binary-safe.
+
+Related functions:
+
+strtolower() - converts a string to lowercase
+lcfirst() - converts the first character of a string to lowercase
+ucfirst() - converts the first character of a string to uppercase
+ucwords() - converts the first character of each word in a string to uppercase';
+
+print strtoupper ($text);
+
+
+?>
+
