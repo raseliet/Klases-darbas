3cc041c4e59ab6efeb857a84aadb3a9eb3e39f4f
raseliet <54890928+raseliet@users.noreply.github.com>
2019-09-05 12:00:17 +0300

    Keiksmazodziu pakeisti *** simboliais.

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
-print str_replace(" ","","converts the first character of a string to lowercase");
+$text =  'Keiksmais isliejamas pasipiktinimas ar atsikertama izeidimu, bet nelinkima pikto. 
+Dazniausiai prisegami pazyminiai senas, kuris reiskia ne tiek amziu, kiek apskritai niekinama vertinima.
+Pavyzdziui: prakeiktas asilas, prakeikta gyvate, rupus miltai.';
+
+$keiksmazodziai = [
+    
+'1' => 'prakeiktas asilas',
+'2' => 'prakeikta gyvate',
+'3' => 'rupus miltai',
+    
+];
+
+$pakeitimai = [
+    '1' => '***',
+    '2' => '***',
+    '3' => '***',
+];
+
+
+$text3 = str_ireplace($keiksmazodziai, $pakeitimai, $text);
+print $text3;
 
 ?>
