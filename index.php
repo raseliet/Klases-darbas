38543126f875dc945a9c01eb63d22fe46fde0906
raseliet <54890928+raseliet@users.noreply.github.com>
2019-09-05 10:32:20 +0300

    php funkcija panaikinti tarpus tarp zodziu

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
 
-$text = 'Convert all characters to uppercase.The strtoupper() function converts a string to uppercase.
-
-Note: This function is binary-safe.
-
-Related functions:
-
-strtolower() - converts a string to lowercase
-lcfirst() - converts the first character of a string to lowercase
-ucfirst() - converts the first character of a string to uppercase
-ucwords() - converts the first character of each word in a string to uppercase';
-
-print strtoupper ($text);
-
+print str_replace(" ","","converts the first character of a string to lowercase");
 
 ?>
-
